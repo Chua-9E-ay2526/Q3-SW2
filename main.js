9e538abdc62493a165ad540703e80ca5a3830997
Chua-9E-ay2526 <chua.cm@obmontessori.edu.ph>
2026-02-20 11:29:36 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function buttonDiscount (){
+    let theBudget = document.getElementsById(budget).value;
+    let theDiscount = Number(origBudget * 0.2);
+
+    document.getElementById().innerHTML='<div class="container"></div>'
+}
